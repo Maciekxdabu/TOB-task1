1719e58791678fde078e118112399c55fbd0a755
Maciej Domański <maciekxdabu@gmail.com>
2024-11-12 15:35:38 +0100

    fix XSS

--- Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
+++ Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
@@ -7,6 +7,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -14,6 +16,8 @@ import java.time.LocalDate;
 public class CreateStudentDTO {
 
     @NotEmpty(message = "{NotEmpty.name}")
+    @Size(min = 1, max = 25, message = "{Size.name}")
+    @Pattern(regexp = "^[A-Z][a-z]*$", message = "{Pattern.name}")
     private String name;
 
     @Email
@@ -28,20 +32,26 @@ public class CreateStudentDTO {
     private String zipCode;
 
     @NotEmpty(message = "{NotEmpty.street}")
+    @Pattern(regexp = "^[A-Za-z0-9]*$", message = "{Pattern.street}")
     private String street;
 
     @NotEmpty(message = "{NotEmpty.number}")
+    @Pattern(regexp = "^[1-9][0-9]*$", message = "{Pattern.number}")
     private String number;
 
+    @Pattern(regexp = "^[A-Za-z0-9]*$", message = "{Pattern.complement}")
     private String complement;
 
     @NotEmpty(message = "{NotEmpty.district}")
+    @Pattern(regexp = "^[A-Za-z0-9]*$", message = "{Pattern.district}")
     private String district;
 
     @NotEmpty(message = "{NotEmpty.city}")
+    @Pattern(regexp = "^[A-Za-z0-9]*$", message = "{Pattern.city}")
     private String city;
 
     @NotEmpty(message = "{NotEmpty.state}")
+    @Pattern(regexp = "^[A-Za-z0-9]*$", message = "{Pattern.state}")
     private String state;
 
 }
